b0732188fcda2b75928e1b9864510da79c9a9a32
RACHID OUMAHNOU <rachidoumahnou14@gmail.com>
2023-03-28 09:59:19 +0000

    login section

--- AbsentManagementSystem/src/app/app-routing.module.ts
+++ AbsentManagementSystem/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : '' , redirectTo : '' , pathMatch: 'full'},
+  {path : 'login' , component : LoginComponent},
+  {path : 'home' , component : HomeComponent},
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
